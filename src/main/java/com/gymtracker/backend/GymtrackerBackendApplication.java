5cdb68afb186aec66d16354aa1f0c6da7c75d947
Luís de Almeida <luisfca@outlook.pt>
2026-02-24 17:50:40 +0000

    Initial Commit

---
+++ src/main/java/com/gymtracker/backend/GymtrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gymtracker.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymtrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymtrackerBackendApplication.class, args);
+	}
+
+}
